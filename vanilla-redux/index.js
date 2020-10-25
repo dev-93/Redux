ee7024740f7ffa2565eb51293e1d4406a285ede0
dev-93 <cr000@naver.com>
2020-10-25 18:51:19 +0900

    액션 타입과 액션 생성함수 정의

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+// action = 상태에 변화를 일으키는 것
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+// action creator = action 객체를 만드는 action 생성 함수
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH});
+const increase = (difference) => ({ type: INCREASE, difference});
+const decrease = () => ({ type: DECREASE});
