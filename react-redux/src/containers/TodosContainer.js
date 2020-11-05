c296027ec55001d5a077ace92b57d568d0044ccd
dev-93 <cr000@naver.com>
2020-11-06 01:59:59 +0900

    todos container 생성

---
+++ react-redux/src/containers/TodosContainer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+import Todos from "../components/Todos";
+import { changeInput, insert, toggle, remove } from "../modules/counter";
+const TodosContainer = ({ input, todos, changeInput, insert, toggle, remove }) => {
+    return (
+        <Todos
+            input={input}
+            todos={todos}
+            onChangeInput={changeInput}
+            onInsert={insert}
+            onRemove={remove}
+            onToggle={toggle}
+        />
+    );
+};
+
+export default connect(
+    ({ todos }) => ({
+        input: todos.input,
+        todos: todos.todos,
+    }),
+    {
+        changeInput,
+        insert,
+        toggle,
+        remove,
+    }
+)(TodosContainer);
