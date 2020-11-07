82a19e607aab62743c7723a6ba8db31e1f643b2c
dev-93 <cr000@naver.com>
2020-11-07 17:31:51 +0900

    initialstate add

--- react-redux/src/modules/todos.js
+++ react-redux/src/modules/todos.js
@@ -38,22 +38,25 @@ const initialState = {
 };
 
 // 리듀서 함수
-const todos = handleActions({
-    [CHANGE_INPUT]: (state, action) => ({ ...state, input: action.payload }),
-    [INSERT]: (state, action) => ({
-        ...state,
-        todos: state.todos.concat(action.payload),
-    }),
-    [TOGGLE]: (state, action) => ({
-        ...state,
-        todos: state.todos.map((todo) =>
-            todo.id === action.payload ? { ...todo, done: !todo.done } : todo
-        ),
-    }),
-    [REMOVE]: (state, action) => ({
-        ...state,
-        todos: state.todos.filter((todo) => todo.id !== action.payload),
-    }),
-});
+const todos = handleActions(
+    {
+        [CHANGE_INPUT]: (state, action) => ({ ...state, input: action.payload }),
+        [INSERT]: (state, action) => ({
+            ...state,
+            todos: state.todos.concat(action.payload),
+        }),
+        [TOGGLE]: (state, action) => ({
+            ...state,
+            todos: state.todos.map((todo) =>
+                todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+            ),
+        }),
+        [REMOVE]: (state, action) => ({
+            ...state,
+            todos: state.todos.filter((todo) => todo.id !== action.payload),
+        }),
+    },
+    initialState
+);
 
 export default todos;
